2716cb83a91354aaaab71f41af8479a50280a2a1
Helder Ferreira Fernandes <geral.helder@outlook.pt>
2023-06-19 01:16:00 +0100

    v2.0

--- src/discord_api/discord_client.py
+++ src/discord_api/discord_client.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv
 from discord.client import Client
 from discord.flags import Intents
@@ -8,8 +9,6 @@ from openai_api.openapi_connector import get_chat_gpt_response
 
 load_dotenv()
 
-bot_caller: str = "/GPT"
-
 discord_token = os.environ.get("DISCORD_TOKEN")
 
 class CustomDiscordClient(Client):
@@ -17,17 +16,45 @@ class CustomDiscordClient(Client):
         print(f"Estou conectado ao Discord com o user {self.user}")
 
     async def on_message(self, message: Message):
-        try:
-            message_content: str = message.content
-            if bot_caller in message_content:
-                prompt: str = message_content.split(bot_caller)[1]
-                print(f"----------------Prompt Message: {prompt} ----------------")
-                chat_gpt_response: str = get_chat_gpt_response(question=prompt)
-                if chat_gpt_response:
-                    await message.channel.send(content=chat_gpt_response)
-        except Exception as e:
-            print(f"Ocorreu um erro ao processar a mensagem: {e}")
+        # ID do canal específico em que o bot deve operar
+        specific_channel_id = "1120135556320460901"  # Substitua isso pelo ID do canal
+
+        # Verifique se a mensagem foi enviada no canal específico e não foi enviada pelo próprio bot
+        if str(message.channel.id) == specific_channel_id and message.author != self.user:
+            print(f"Recebi uma mensagem: {message.content}")  # Adicionar esta linha
+            prompt = message.content
+
+            # Carregar histórico de conversa do arquivo
+            try:
+                with open("conversation_history.json", "r") as file:
+                    conversation_history = json.load(file)
+                    if not conversation_history:
+                        conversation_history = []
+            except (FileNotFoundError, json.JSONDecodeError):
+                conversation_history = []
+
+            # Obter resposta do chatbot
+            print("Chamando get_chat_gpt_response...")  # Adicionar esta linha
+            chat_gpt_response = get_chat_gpt_response(question=prompt, history=conversation_history)
+            print(f"Resposta recebida: {chat_gpt_response}")  # Adicionar esta linha
+
+            # Adicionar resposta ao histórico
+            if chat_gpt_response:
+                conversation_history.append({"User": prompt, "Bot": chat_gpt_response})
+
+                # Salvar o histórico atualizado no arquivo
+                with open("conversation_history.json", "w") as file:
+                    json.dump(conversation_history, file)
+
+                # Enviar resposta para o canal Discord
+                await message.channel.send(content=chat_gpt_response)
+            else:
+                print("Nenhuma resposta recebida do chatbot.")  # Adicionar esta linha
+        else:
+            print("Mensagem ignorada (não está no canal específico ou foi enviada pelo bot).")  # Adicionar esta linha
+
 
 intents = Intents.default()
 intents.message_content = True
-custom_discord_client: CustomDiscordClient = CustomDiscordClient(intents=intents)
+custom_discord_client = CustomDiscordClient(intents=intents)
+custom_discord_client.run(discord_token)
--- src/openai_api/openapi_connector.py
+++ src/openai_api/openapi_connector.py
@@ -7,33 +7,30 @@ load_dotenv()
 
 openai.api_key = os.environ.get("CHAT_GPT_API_KEY")
 
-# Manter um histórico de mensagens para simular uma conversa contínua
-conversation_history = [
-    {"role": "system", "content": "Você é um assistente de chat."}
-]
+def get_chat_gpt_response(question: str, history: List[Dict[str, str]]) -> str | None:
+    # Construir a lista de mensagens com histórico de conversa
+    messages = [{"role": "system", "content": "Você é um chatbot útil."}]
+    for entry in history:
+        messages.append({"role": "user", "content": entry["User"]})
+        messages.append({"role": "assistant", "content": entry["Bot"]})
+    messages.append({"role": "user", "content": question})
 
-def get_chat_gpt_response(question: str) -> str | None:
+    # Fazer a chamada da API OpenAI
     try:
-        # Adicionar a nova mensagem do usuário ao histórico da conversa
-        conversation_history.append({"role": "user", "content": question})
-
-        # Usando o endpoint v1/chat/completions para modelos de chat
         response_from_chat_gpt = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=conversation_history
+            model="gpt-3.5-turbo-0613",
+            messages=messages,
+            max_tokens=1000
         )
-
-        # Extraindo a resposta do modelo
-        choices = response_from_chat_gpt.get("choices", [])
-        if choices:
-            message = choices[0].get("message", {})
-            text = message.get("content", None)
-
-            # Adicionar a resposta do modelo ao histórico da conversa
-            conversation_history.append({"role": "assistant", "content": text})
-
-            return text
     except Exception as e:
-        print(f"Ocorreu um erro: {e}")
+        return None
 
+    # Extrair a resposta do chatbot
+    if response_from_chat_gpt:
+        choices = response_from_chat_gpt.get("choices", None)
+        if choices and len(choices) > 0:
+            message = choices[0].get("message", None)
+            if message:
+                text = message.get("content", None)
+                return text
     return None
